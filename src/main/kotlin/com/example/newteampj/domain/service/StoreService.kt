c01d98f6275897a27025b2c964f8a2f73035020c
happynslow <happyx2day2@gmail.com>
2024-02-22 15:10:09 +0900

    다른코드 사용

--- src/main/kotlin/com/example/newteampj/domain/service/StoreService.kt
+++ src/main/kotlin/com/example/newteampj/domain/service/StoreService.kt
@@ -3,12 +3,13 @@ package com.example.newteampj.domain.service
 import com.example.newteampj.domain.dto.StoreResponse
 import com.example.newteampj.domain.repository.storeRepository
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
-class StoreService {
+interface StoreService {
 
-    fun getStoreList(): List<StoreResponse> {}
-
+    //fun getStoreList(): List<StoreResponse> {}
+    fun save(file: MultipartFile)
 
 
     }
